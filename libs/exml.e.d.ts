f9e989cfd0665a6c952cd8b52a590520ccf4d84b
cooluid <173000@qq.com>
2018-04-26 01:24:15 +0800

    配置资源

---
+++ libs/exml.e.d.ts
@@ -0,0 +1,70 @@
+declare module skins{
+	class ButtonSkin extends eui.Skin{
+	}
+}
+declare module skins{
+	class CheckBoxSkin extends eui.Skin{
+	}
+}
+declare module skins{
+	class HScrollBarSkin extends eui.Skin{
+	}
+}
+declare module skins{
+	class HSliderSkin extends eui.Skin{
+	}
+}
+declare module skins{
+	class ItemRendererSkin extends eui.Skin{
+	}
+}
+declare module skins{
+	class PanelSkin extends eui.Skin{
+	}
+}
+declare module skins{
+	class ProgressBarSkin extends eui.Skin{
+	}
+}
+declare module skins{
+	class RadioButtonSkin extends eui.Skin{
+	}
+}
+declare module skins{
+	class ScrollerSkin extends eui.Skin{
+	}
+}
+declare module skins{
+	class TextInputSkin extends eui.Skin{
+	}
+}
+declare module skins{
+	class ToggleSwitchSkin extends eui.Skin{
+	}
+}
+declare module skins{
+	class VScrollBarSkin extends eui.Skin{
+	}
+}
+declare module skins{
+	class VSliderSkin extends eui.Skin{
+	}
+}
+declare class FrameSkin extends eui.Skin{
+}
+declare class PlayerSkin extends eui.Skin{
+}
+declare class StoneSkin extends eui.Skin{
+}
+declare class MainSkin extends eui.Skin{
+}
+declare class WelcomeSkin extends eui.Skin{
+}
+declare class NostopMainItemSkin extends eui.Skin{
+}
+declare class NostopPlayerWinSkin extends eui.Skin{
+}
+declare class NostopWinSkin extends eui.Skin{
+}
+declare class Button_01 extends eui.Skin{
+}
